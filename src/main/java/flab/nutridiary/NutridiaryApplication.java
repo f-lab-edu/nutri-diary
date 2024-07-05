10728240520bce26fc9d666c2602d3522546eb07
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-05 14:12:16 +0900

    Create spring boot project

---
+++ src/main/java/flab/nutridiary/NutridiaryApplication.java
@@ -0,0 +1,13 @@
+package flab.nutridiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutridiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutridiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flab/nutridiary/NutridiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.nutridiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutridiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
