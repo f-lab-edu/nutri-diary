cc8bf279f2435f93fc0097a680e4066403a63928
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-29 08:28:42 +0900

    test: 상품 중복 등록 테스트 코드 작성

--- src/test/java/flab/nutridiary/product/controller/ProductControllerTest.java
+++ src/test/java/flab/nutridiary/product/controller/ProductControllerTest.java
@@ -1,7 +1,9 @@
 package flab.nutridiary.product.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import flab.nutridiary.product.domain.Product;
 import flab.nutridiary.product.dto.NewProductRequest;
+import flab.nutridiary.product.repository.ProductRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +31,9 @@ class ProductControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @DisplayName("상품 등록 컨트롤러 테스트.")
     @Test
@@ -58,4 +63,38 @@ class ProductControllerTest {
                 .andExpect(jsonPath("$.data.productId").exists());
     }
 
+    @DisplayName("상품 등록 실패 컨트롤러 테스트.")
+    @Test
+    void addProductEx() throws Exception {
+        // given
+        Product prevProduct = Product.builder()
+                .productName("촉촉한 초코칩 쿠키")
+                .productCorp("오리온")
+                .build();
+        productRepository.save(prevProduct);
+
+
+        NewProductRequest newProductRequest = NewProductRequest.builder()
+                .productName("촉촉한초코칩쿠키")
+                .corpName("오리온")
+                .servingSize(1)
+                .servingUnit("컵")
+                .servingWeightGram(90)
+                .calories(BigDecimal.valueOf(120))
+                .carbohydrate(BigDecimal.valueOf(15.5))
+                .protein(BigDecimal.valueOf(3.5))
+                .fat(BigDecimal.valueOf(5.5))
+                .build();
+
+        // when
+        mockMvc.perform(
+                        post("/product/new")
+                                .content(objectMapper.writeValueAsString(newProductRequest))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.statusCode").value(400))
+                .andExpect(jsonPath("$.message").value("이미 등록된 상품입니다."));
+    }
 }
---
+++ src/test/java/flab/nutridiary/product/service/ProductValidatorTest.java
@@ -0,0 +1,46 @@
+package flab.nutridiary.product.service;
+
+import flab.nutridiary.product.domain.Product;
+import flab.nutridiary.product.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@ActiveProfiles("test")
+@Transactional
+@SpringBootTest
+class ProductValidatorTest {
+
+    @Autowired
+    private ProductValidator productValidator;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @DisplayName("중복된 상품이 등록되면 예외가 발생한다.")
+    @Test
+    void validate() throws Exception {
+        // given
+        Product product = Product.builder()
+                .productName("촉촉한   초코칩  쿠키")
+                .productCorp(" 오리온")
+                .build();
+        productRepository.save(product);
+
+        // when
+        Product product2 = Product.builder()
+                .productName("촉촉한 초코칩쿠키")
+                .productCorp("오리온")
+                .build();
+
+        // then
+        Assertions.assertThrows(ProductDuplicatedException.class, () -> {
+            productValidator.validate(product2);
+        });
+    }
+
+}
