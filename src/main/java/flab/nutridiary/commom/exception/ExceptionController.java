2d0fa875776ad56128a593aa1fe732c8e8d027b7
koo995 <107671886+koo995@users.noreply.github.com>
2024-08-17 22:59:08 +0900

    feat: 유효성 검증 실패 response 반환

--- src/main/java/flab/nutridiary/commom/exception/ExceptionController.java
+++ src/main/java/flab/nutridiary/commom/exception/ExceptionController.java
@@ -3,9 +3,15 @@ package flab.nutridiary.commom.exception;
 import flab.nutridiary.commom.dto.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static flab.nutridiary.commom.exception.StatusConst.*;
 
 @Slf4j
 @RestControllerAdvice
@@ -22,5 +28,14 @@ public class ExceptionController {
     public ApiResponse<Void> SystemExceptionHandler(SystemException e) {
         log.info("SystemException : {}", e.getMessage());
         return ApiResponse.sysException();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiResponse<Map<String, String>> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return ApiResponse.bizException(VALIDATION_CHECK_FAIL.getStatusCode(), VALIDATION_CHECK_FAIL.getMessage(), errors);
     }
 }
