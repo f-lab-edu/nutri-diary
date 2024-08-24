2a65e02cdf2497994b76f069d12588ee445b5c96
koo995 <107671886+koo995@users.noreply.github.com>
2024-08-25 01:54:57 +0900

    refactor: 영양성분의 Json 필드 대신 varchar 사용

--- src/test/java/flab/nutridiary/config/TestJdbcConfig.java
+++ src/test/java/flab/nutridiary/config/TestJdbcConfig.java
@@ -6,14 +6,12 @@ import flab.nutridiary.config.converter.JsonToNutritionTestConverter;
 import flab.nutridiary.config.converter.NutritionFactsToJsonTestConverter;
 import flab.nutridiary.config.converter.NutritionToJsonTestConverter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jdbc.core.convert.JdbcCustomConversions;
 
 import java.util.Arrays;
 
-@Configuration
-@Profile("test")
+//@Profile("test")
+//@Configuration
 public class TestJdbcConfig {
 
     @Bean
