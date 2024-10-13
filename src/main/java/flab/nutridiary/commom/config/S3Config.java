1e6cf3ab7b58c60191c4b92f5450a60de3a5749c
koo995 <107671886+koo995@users.noreply.github.com>
2024-10-13 21:34:57 +0900

    refactor: S3Config 설정값 환경변수로 주입

--- src/main/java/flab/nutridiary/commom/config/S3Config.java
+++ src/main/java/flab/nutridiary/commom/config/S3Config.java
@@ -6,7 +6,9 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile({"prod", "dev"})
 @Configuration
 public class S3Config {
 
