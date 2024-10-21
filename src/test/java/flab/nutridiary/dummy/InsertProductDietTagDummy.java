9f7055fe010242a58fc28215e02220f369d17b5f
koo995 <107671886+koo995@users.noreply.github.com>
2024-10-21 21:36:25 +0900

    chore: 더미데이터 삽입 코드 주석처리

--- src/test/java/flab/nutridiary/dummy/InsertProductDietTagDummy.java
+++ src/test/java/flab/nutridiary/dummy/InsertProductDietTagDummy.java
@@ -1,63 +1,52 @@
 package flab.nutridiary.dummy;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
-@SpringBootTest
-public class InsertProductDietTagDummy {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    private final int batchSize = 1000;
-    private final int threadCount = 5;
-    private final int totalDataCount = 20000000;
-
-    private final String productDietTagSQL = "INSERT INTO product_diet_tag (product_id, diet_tag_id, created_at, updated_at) VALUES (?, ?, ?, ?)";
-
-    @Test
-    public void insertProductDataInParallel() throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
-        int chunkSize = totalDataCount / threadCount;
-
-        for (int i = 0; i < threadCount; i++) {
-            int start = i * chunkSize + 1;
-            int end = (i == threadCount - 1) ? totalDataCount : (i + 1) * chunkSize;
-
-            executorService.submit(() -> insertChunk(productDietTagSQL, start, end));
-        }
-
-        executorService.shutdown();
-        executorService.awaitTermination(1, TimeUnit.HOURS); // 스레드가 모두 종료될 때까지 대기
-    }
-
-    public void insertChunk(String sql, int start, int end) {
-        List<Object[]> batchArgs = new ArrayList<>();
-
-        for (int i = start; i <= end; i++) {
-            int productId = (i % 8) * 100000 + i % 100000;
-            int dietTagId = i % 10 + 1;
-            String createdAt = "2024-09-01 00:00:00";
-            String updatedAt = "2024-09-01 00:00:00";
-            batchArgs.add(new Object[]{productId, dietTagId, createdAt, updatedAt});
-
-            if (batchArgs.size() % batchSize == 0) {
-                jdbcTemplate.batchUpdate(sql, batchArgs);
-                batchArgs.clear();
-            }
-        }
-
-        if (!batchArgs.isEmpty()) {
-            jdbcTemplate.batchUpdate(sql, batchArgs);
-        }
-
-        System.out.println("스레드가 " + start + "부터 " + end + "까지 데이터를 삽입했습니다.");
-    }
-}
+//@SpringBootTest
+//public class InsertProductDietTagDummy {
+//    @Autowired
+//    private JdbcTemplate jdbcTemplate;
+//
+//    private final int batchSize = 1000;
+//    private final int threadCount = 5;
+//    private final int totalDataCount = 20000000;
+//
+//    private final String productDietTagSQL = "INSERT INTO product_diet_tag (product_id, diet_tag_id, created_at, updated_at) VALUES (?, ?, ?, ?)";
+//
+//    @Test
+//    public void insertProductDataInParallel() throws InterruptedException {
+//        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+//        int chunkSize = totalDataCount / threadCount;
+//
+//        for (int i = 0; i < threadCount; i++) {
+//            int start = i * chunkSize + 1;
+//            int end = (i == threadCount - 1) ? totalDataCount : (i + 1) * chunkSize;
+//
+//            executorService.submit(() -> insertChunk(productDietTagSQL, start, end));
+//        }
+//
+//        executorService.shutdown();
+//        executorService.awaitTermination(1, TimeUnit.HOURS); // 스레드가 모두 종료될 때까지 대기
+//    }
+//
+//    public void insertChunk(String sql, int start, int end) {
+//        List<Object[]> batchArgs = new ArrayList<>();
+//
+//        for (int i = start; i <= end; i++) {
+//            int productId = (i % 8) * 100000 + i % 100000;
+//            int dietTagId = i % 10 + 1;
+//            String createdAt = "2024-09-01 00:00:00";
+//            String updatedAt = "2024-09-01 00:00:00";
+//            batchArgs.add(new Object[]{productId, dietTagId, createdAt, updatedAt});
+//
+//            if (batchArgs.size() % batchSize == 0) {
+//                jdbcTemplate.batchUpdate(sql, batchArgs);
+//                batchArgs.clear();
+//            }
+//        }
+//
+//        if (!batchArgs.isEmpty()) {
+//            jdbcTemplate.batchUpdate(sql, batchArgs);
+//        }
+//
+//        System.out.println("스레드가 " + start + "부터 " + end + "까지 데이터를 삽입했습니다.");
+//    }
+//}
--- src/test/java/flab/nutridiary/dummy/InsertReviewDummy.java
+++ src/test/java/flab/nutridiary/dummy/InsertReviewDummy.java
@@ -11,55 +11,55 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-@SpringBootTest
-public class InsertReviewDummy {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    private final int batchSize = 1000;
-    private final int threadCount = 5;
-    private final int totalDataCount = 10000000;
-
-    private final String reviewSQL = "INSERT INTO review (product_id, member_id, content, rating, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)";
-
-    @Test
-    public void insertProductDataInParallel() throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
-        int chunkSize = totalDataCount / threadCount;
-
-        for (int i = 0; i < threadCount; i++) {
-            int start = i * chunkSize + 1;
-            int end = (i == threadCount - 1) ? totalDataCount : (i + 1) * chunkSize;
-
-            executorService.submit(() -> insertChunk(reviewSQL, start, end));
-        }
-
-        executorService.shutdown();
-        executorService.awaitTermination(1, TimeUnit.HOURS); // 스레드가 모두 종료될 때까지 대기
-    }
-
-    public void insertChunk(String sql, int start, int end) {
-        List<Object[]> batchArgs = new ArrayList<>();
-
-        for (int i = start; i <= end; i++) {
-            int productId = (i % 8) * 100000 + i % 100000;
-            int memberId = i;
-            String content = i + "content " + i;
-            int rating = i % 5 + 1;
-            String createdAt = "2024-09-01 00:00:00";
-            String updatedAt = "2024-09-01 00:00:00";
-            batchArgs.add(new Object[]{productId, memberId, content, rating, createdAt, updatedAt});
-
-            if (batchArgs.size() % batchSize == 0) {
-                jdbcTemplate.batchUpdate(sql, batchArgs);
-                batchArgs.clear();
-            }
-        }
-
-        if (!batchArgs.isEmpty()) {
-            jdbcTemplate.batchUpdate(sql, batchArgs);
-        }
-
-        System.out.println("스레드가 " + start + "부터 " + end + "까지 데이터를 삽입했습니다.");
-    }
-}
+//@SpringBootTest
+//public class InsertReviewDummy {
+//    @Autowired
+//    private JdbcTemplate jdbcTemplate;
+//
+//    private final int batchSize = 1000;
+//    private final int threadCount = 5;
+//    private final int totalDataCount = 10000000;
+//
+//    private final String reviewSQL = "INSERT INTO review (product_id, member_id, content, rating, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)";
+//
+//    @Test
+//    public void insertProductDataInParallel() throws InterruptedException {
+//        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+//        int chunkSize = totalDataCount / threadCount;
+//
+//        for (int i = 0; i < threadCount; i++) {
+//            int start = i * chunkSize + 1;
+//            int end = (i == threadCount - 1) ? totalDataCount : (i + 1) * chunkSize;
+//
+//            executorService.submit(() -> insertChunk(reviewSQL, start, end));
+//        }
+//
+//        executorService.shutdown();
+//        executorService.awaitTermination(1, TimeUnit.HOURS); // 스레드가 모두 종료될 때까지 대기
+//    }
+//
+//    public void insertChunk(String sql, int start, int end) {
+//        List<Object[]> batchArgs = new ArrayList<>();
+//
+//        for (int i = start; i <= end; i++) {
+//            int productId = (i % 8) * 100000 + i % 100000;
+//            int memberId = i;
+//            String content = i + "content " + i;
+//            int rating = i % 5 + 1;
+//            String createdAt = "2024-09-01 00:00:00";
+//            String updatedAt = "2024-09-01 00:00:00";
+//            batchArgs.add(new Object[]{productId, memberId, content, rating, createdAt, updatedAt});
+//
+//            if (batchArgs.size() % batchSize == 0) {
+//                jdbcTemplate.batchUpdate(sql, batchArgs);
+//                batchArgs.clear();
+//            }
+//        }
+//
+//        if (!batchArgs.isEmpty()) {
+//            jdbcTemplate.batchUpdate(sql, batchArgs);
+//        }
+//
+//        System.out.println("스레드가 " + start + "부터 " + end + "까지 데이터를 삽입했습니다.");
+//    }
+//}
