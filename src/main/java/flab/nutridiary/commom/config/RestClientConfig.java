69df69b7b9b7f4d94eb65fda453ef0fc86c02bc9
koo995 <107671886+koo995@users.noreply.github.com>
2024-12-20 16:44:58 +0900

    Elastic Search 연결 실패

---
+++ src/main/java/flab/nutridiary/commom/config/RestClientConfig.java
@@ -0,0 +1,42 @@
+package flab.nutridiary.commom.config;
+
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.data.client.orhlc.AbstractOpenSearchConfiguration;
+import org.opensearch.data.client.orhlc.ClientConfiguration;
+import org.opensearch.data.client.orhlc.RestClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@EnableElasticsearchRepositories(basePackages = "flab.nutridiary.search")
+@Configuration
+public class RestClientConfig extends AbstractOpenSearchConfiguration {
+
+    @Value("${spring.elasticsearch.url}")
+    private String url;
+
+    @Value("${spring.elasticsearch.port}")
+    private int port;
+
+    @Value("${spring.elasticsearch.username}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password}")
+    private String password;
+
+    @Override
+    @Bean
+    public RestHighLevelClient
+    opensearchClient() {
+
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo(url)
+                .usingSsl()
+                .withBasicAuth(username, password)
+                .withClientConfigurer(clientConfigurer -> clientConfigurer)
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
